dee38f65b4a31bb5e088260c0d2ba14308cea454
Deepesh Gupta <deepesh@findcoder.io>
2023-05-17 15:17:07 +0530

    defined constants

---
+++ src/constant.ts
@@ -0,0 +1,5 @@
+export const APPWRITE_URL = String(process.env.REACT_APP_APPWRITE_URL);
+export const APPWRITE_PROJECT_ID = String(process.env.REACT_APP_APPWRITE_PROJECT_ID);
+export const APPWRITE_DATABASE_ID = String(process.env.REACT_APP_APPWRITE_DATABASE_ID);
+export const APPWRITE_COLLECTION_ID = String(process.env.REACT_APP_APPWRITE_COLLECTION_ID);
+export const APPWRITE_BUCKET_ID = String(process.env.REACT_APP_APPWRITE_BUCKET_ID);
